12e82267059ca15f96524c1ce7c883156483163b
1nyeong <inyeong0726@gmail.com>
2023-01-12 23:26:24 +0900

    search api re

--- src/main/resources/static/js/account/register.js
+++ src/main/resources/static/js/account/register.js
@@ -1,17 +1,17 @@
 window.onload = () => {
-    RegisterApi.getInstance().addRegisterSubmitOnclickEvent();
+    RegisterEvent.getInstance().addRegisterSubmitOnclickEvent();
 }
 
-class RegisterApi{
+class RegisterApi {
     static #instance = null;
     static getInstance() {
-        if(this.#instance == null){
+        if(this.#instance == null) {
             this.#instance = new RegisterApi();
         }
-        return this.#instance
+        return this.#instance;
     }
 
-    register(user){
+    register(user) {
         $.ajax({
             async: false,
             type: "post",
@@ -19,91 +19,92 @@ class RegisterApi{
             contentType: "application/json",
             data: JSON.stringify(user),
             dataType: "json",
-            success: response=> {
+            success: response => {
                 console.log(response);
+                alert("회원가입 완료. 로그인 페이지로 이동합니다.");
+                location.replace("/account/login");
             },
             error: error => {
                 console.log(error);
-                RegisterService.getInstance().setErrorMessage(error.responseJson.data);
+                RegisterService.getInstance().setErrorMessage(error.responseJSON.data);
             }
-        })
+        });
     }
 }
 
-class RegisterService{
+
+class RegisterService {
     static #instance = null;
     static getInstance() {
-        if(this.#instance == null){
+        if(this.#instance == null) {
             this.#instance = new RegisterService();
         }
-        return this.#instance
+        return this.#instance;
     }
 
-    setErrorMessage(errors){
+    setErrorMessage(errors) {
         const registerError = document.querySelectorAll(".register-error");
+
         this.#clearErrorMessage();
+
         Object.keys(errors).forEach(error => {
-            if(error == "username"){
+            if(error == "username") {
                 registerError[0].textContent = errors[error];
-            }else if(error == "password"){
+            }else if(error == "password") {
                 registerError[1].textContent = errors[error];
-            }else if(error == "repassword"){
+            }else if(error == "repassword") {
                 registerError[2].textContent = errors[error];
-            }else if(error == "name"){
+            }else if(error == "name") {
                 registerError[3].textContent = errors[error];
-            }else if(error == "email"){
+            }else if(error == "email") {
                 registerError[4].textContent = errors[error];
             }
-
-        })
+        });
     }
 
-    #clearErrorMessage(){
+    #clearErrorMessage() {
         const registerError = document.querySelectorAll(".register-error");
         registerError.forEach(error => {
             error.textContent = "";
         });
     }
-
 }
 
-class RegisterEvent{
+
+class RegisterEvent {
     static #instance = null;
     static getInstance() {
-        if(this.#instance == null){
+        if(this.#instance == null) {
             this.#instance = new RegisterEvent();
         }
-        return this.#instance
+        return this.#instance;
     }
 
-    addRegisterSubmitOnclickEvent(){
+    addRegisterSubmitOnclickEvent() {
         const registerSubmit = document.querySelector(".register-submit");
 
-        registerSubmit.Onclick = () => {
-            const usernameValue = document.querySelectorAll("register-inputs")[0].Value;
-            const passwordValue = document.querySelectorAll("register-inputs")[1].Value;
-            const repasswordValue = document.querySelectorAll("register-inputs")[2].Value;
-            const nameValue = document.querySelectorAll("register-inputs")[3].Value;
-            const emailValue = document.querySelectorAll("register-inputs")[4].Value;
-
-            console.log(usernameValue);
+        registerSubmit.onclick = () => {
+            const usernameValue = document.querySelectorAll(".register-inputs")[0].value;
+            const passwordValue = document.querySelectorAll(".register-inputs")[1].value;
+            const repasswordValue = document.querySelectorAll(".register-inputs")[2].value;
+            const nameValue = document.querySelectorAll(".register-inputs")[3].value;
+            const emailValue = document.querySelectorAll(".register-inputs")[4].value;
 
             const user = new User(usernameValue, passwordValue, repasswordValue, nameValue, emailValue);
 
             RegisterApi.getInstance().register(user);
         }
     }
-
 }
 
-class User{
+class User {
     username = null;
     password = null;
     repassword = null;
     name = null;
     email = null;
 
-    constructor(username, password, repassword, name, email){
+    constructor(username, password, repassword, name, email) {
         this.username = username;
         this.password = password;
         this.repassword = repassword;
